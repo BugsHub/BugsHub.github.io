dd42abbf38656b7df802898b330d94eb7d08cc4f
BugsHub <91665285+BugsHub@users.noreply.github.com>
2021-10-15 13:15:57 +0800

    Update bullets.js

--- bullets.js
+++ bullets.js
@@ -1,4 +1,2 @@
-var myinfo1,myinfo2;
-myinfo2="点击下方访问BugsHub.github.io";
-myinfo1="声明：图片接入Bing每日一图，出现的内容对于本网站作者同样“惊喜”。";
-alert(myinfo1+myinfo2)
+alert("声明：图片接入Bing每日一图，出现的内容对于本网站作者同样“惊喜”。"
+      "点击下方访问BugsHub.github.io");
